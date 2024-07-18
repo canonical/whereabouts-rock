78a0ba72546db39993feaad37c2e8c06e6194ed9
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-07-18 17:37:35 +0000

    sanity: Uses k8s-test-harness utils

--- tests/sanity/test_whereabouts.py
+++ tests/sanity/test_whereabouts.py
@@ -2,9 +2,9 @@
 # Copyright 2024 Canonical, Ltd.
 #
 import os
-import subprocess
 
 import pytest
+from k8s_test_harness.util import docker_util
 
 ROCK_EXPECTED_FILES = [
     "/host",
@@ -14,24 +14,6 @@ ROCK_EXPECTED_FILES = [
 ]
 
 
-def _run_in_docker(image, check_exit_code, *command):
-    return subprocess.run(
-        [
-            "docker",
-            "run",
-            "--rm",
-            "-i",
-            "--entrypoint",
-            command[0],
-            image,
-            *command[1:],
-        ],
-        check=check_exit_code,
-        capture_output=True,
-        text=True,
-    )
-
-
 @pytest.mark.abort_on_fail
 def test_whereabouts_rock():
     """Test Whereabouts rock."""
@@ -39,17 +21,17 @@ def test_whereabouts_rock():
     image = os.getenv(image_variable)
     assert image is not None, f"${image_variable} is not set"
     # check rock filesystem
-    _run_in_docker(image, True, "ls", "-la", *ROCK_EXPECTED_FILES)
+    docker_util.ensure_image_contains_paths(image, ROCK_EXPECTED_FILES)
 
     # check binary name and version.
-    process = _run_in_docker(image, True, "/whereabouts", "version")
+    process = docker_util.run_in_docker(image, True, "/whereabouts", "version")
     output = process.stderr
     assert "whereabouts" in output and "0.6.3" in output
 
     # check other binary. It expects KUBERNETES_SERVICE_HOST to be defined.
-    process = _run_in_docker(image, False, "/ip-control-loop")
+    process = docker_util.run_in_docker(image, False, "/ip-control-loop")
     assert "KUBERNETES_SERVICE_HOST" in process.stderr
 
     # check script. It expects serviceaccount token to exist.
-    process = _run_in_docker(image, False, "/install-cni.sh")
+    process = docker_util.run_in_docker(image, False, "/install-cni.sh")
     "cat: /var/run/secrets/kubernetes.io/serviceaccount/token: No such file or directory" in process.stderr
