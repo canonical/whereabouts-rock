f73a54f4dc37d7fb77083416ca6594e3065a42b0
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-07-19 11:01:51 +0300

    Adds other versions of Whereabouts (#9)
    Added support for versions 0.5.4 and 0.6.1. Added rockcraft.yaml
    files based on their Dockerfiles.
    
    Adds /host folder in the rocks. The /install-cni.sh script requires
    this folder. It does mkdir it, but the bitnami helm chart creates
    the daemonsets with read-only filesystem, making it impossible to
    create the folder.

--- tests/sanity/test_whereabouts.py
+++ tests/sanity/test_whereabouts.py
@@ -2,9 +2,9 @@
 # Copyright 2024 Canonical, Ltd.
 #
 import os
-import subprocess
 
 import pytest
+from k8s_test_harness.util import docker_util
 
 ROCK_EXPECTED_FILES = [
     "/host",
@@ -14,42 +14,42 @@ ROCK_EXPECTED_FILES = [
 ]
 
 
-def _run_in_docker(image, check_exit_code, *command):
-    return subprocess.run(
-        [
-            "docker",
-            "run",
-            "--rm",
-            "-i",
-            "--entrypoint",
-            command[0],
-            image,
-            *command[1:],
-        ],
-        check=check_exit_code,
-        capture_output=True,
-        text=True,
-    )
-
-
-@pytest.mark.abort_on_fail
-def test_whereabouts_rock():
+def _test_whereabouts_rock(image_variable, expected_files):
     """Test Whereabouts rock."""
-    image_variable = "ROCK_WHEREABOUTS"
     image = os.getenv(image_variable)
     assert image is not None, f"${image_variable} is not set"
+
     # check rock filesystem
-    _run_in_docker(image, True, "ls", "-la", *ROCK_EXPECTED_FILES)
+    docker_util.ensure_image_contains_paths(image, expected_files)
 
     # check binary name and version.
-    process = _run_in_docker(image, True, "/whereabouts", "version")
+    version = docker_util.get_image_version(image)
+    process = docker_util.run_in_docker(image, True, "/whereabouts", "version")
     output = process.stderr
-    assert "whereabouts" in output and "0.6.3" in output
+    assert "whereabouts" in output and version in output
 
     # check other binary. It expects KUBERNETES_SERVICE_HOST to be defined.
-    process = _run_in_docker(image, False, "/ip-control-loop")
+    process = docker_util.run_in_docker(image, False, "/ip-control-loop")
     assert "KUBERNETES_SERVICE_HOST" in process.stderr
 
     # check script. It expects serviceaccount token to exist.
-    process = _run_in_docker(image, False, "/install-cni.sh")
+    process = docker_util.run_in_docker(image, False, "/install-cni.sh")
     "cat: /var/run/secrets/kubernetes.io/serviceaccount/token: No such file or directory" in process.stderr
+
+    # whereabouts:0.5.4 also has a /ip-reconciler
+    if version == "0.5.4":
+        process = docker_util.run_in_docker(image, False, "/ip-reconciler")
+        expected_message = "failed to instantiate the Kubernetes client"
+        assert expected_message in process.stderr
+
+
+def test_whereabouts_rock_0_6_3():
+    _test_whereabouts_rock("ROCK_WHEREABOUTS_0_6_3", ROCK_EXPECTED_FILES)
+
+
+def test_whereabouts_rock_0_6_1():
+    _test_whereabouts_rock("ROCK_WHEREABOUTS_0_6_1", ROCK_EXPECTED_FILES)
+
+
+def test_whereabouts_rock_0_5_4():
+    _test_whereabouts_rock("ROCK_WHEREABOUTS_0_5_4", ROCK_EXPECTED_FILES + ["/ip-reconciler"])
